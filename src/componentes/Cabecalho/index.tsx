8a415bdd60db98902f55f4c1a92fd9c44efcc3fb
Carlos Lima <carloslima@mpdft.mp.br>
2023-03-09 22:10:50 -0300

    Aula 1 fazendo solo

---
+++ src/componentes/Cabecalho/index.tsx
@@ -0,0 +1,13 @@
+// src/componentes/Cabecalho/index.tsx
+import './estilos.css'
+
+const Cabecalho = () => {
+  return (
+    <header className="cabecalho">
+      <div className="imagem-logo" role="img" aria-label='Logo do Sorteador'></div>
+      <img className='participante' src="/imagens/participante.png" alt="Participante com um presente na mão" />
+    </header>
+  )
+}
+
+export default Cabecalho
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import Cabecalho from './componentes/Cabecalho';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Cabecalho />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
